292a641827ba96a2a038b24a5ac237480063b73e
afhuertass <afhuertass@unal.edu.co>
2019-03-28 19:12:51 +0200

    #

--- ex/ex3/spectral.py
+++ ex/ex3/spectral.py
@@ -26,6 +26,21 @@ def get_degree( M ):
 	 return R
 
 
+def plot_vectors( evectors ):
+
+	evectors = evectors.T
+	# 
+	for i in range( evectors.shape[0] ):
+
+		vec = evectors[ i , : ]
+
+		plt.plot(  range( vec.shape[0] ) , vec  )
+		plt.savefig("./eigenvec_{}.png".format( i ) )
+		plt.clf()
+
+
+	return 
+
 def do_thing( L  , k = 4 , prefix = "" ):
 
 	eigenvalues, eigenvectors = np.linalg.eig( L )
@@ -39,17 +54,11 @@ def do_thing( L  , k = 4 , prefix = "" ):
 	#print( eigenvalues )
 
 	smallest_vectors = eigenvectors [  : , k_smallest  ]
+	plot_vectors( smallest_vectors )
 
-	# L = ( 120  , 120 ) ( 120 , 4 )
-	print("asdadas")
-	print( smallest_vectors.shape )
-	#NewRep = np.matmul( L , smallest_vectors.T )
-
-	#print( smallest_vectors.shape )
-	#print( smallest_vectors[0 , : ])
 	i = 0 
 	j = 1
-	#print(smallest_vectors.shape)
+	# Print all combinations of eigenvectors 
 	combs = list( itertools.combinations( range( smallest_vectors.shape[ 1 ] ) , 2) )
 
 	for  i , j in combs:
@@ -67,6 +76,7 @@ def main( epsilon = 0.5  , A = 8):
 	A = A +1 
 	df = read_data("./exercise3data.csv")
 
+	# get euclidean distance matrix 
 	D = euclidean_distances( df.values , df.values )
 	print( D.shape )
 	print( D[:5 , :5 ].shape  )
@@ -87,34 +97,25 @@ def main( epsilon = 0.5  , A = 8):
 
 	for i in range( D.shape[0] ):
 		distances = D[ i , : ]
-		# eight largestes 
-		#print( distances.min() )
 		indexs = np.argsort( distances )[:A]
-		#print( distances[indexs] )
 		indexs = list( indexs )
-		#print(indexs)
 		indexs.remove( i )
 		
 		W2[ i , indexs ] = 1.0
-		W2[ indexs , i ] = 1.0 
+		W2[ indexs , i ] = 1.0 # for the symetry 
 
 	D1 = get_degree( W1 )
 	D2 = get_degree( W2 )
 
-	#np.fill_diagonal( W1 , 0.0 )
-	
-
-	print( W2[:3, :3] )
-	print( check_symmetric( W1 ) )
+	# Build laplacian matrix
 	L1  = D1 - W1
 	L2 = D2 - W2
+	# the 
 	newX1 = do_thing(L1 , prefix = "L1")
 	newX2 = do_thing(L2 , prefix = "L2")
 
 
 	thing( df , newX1 , newX2 , prefix1 = "e_{}".format( epsilon ) , prefix2 = "A_{}".format( A ) )
-	## Clustering 
-	# kmeans
 
 def thing( df , newX1 , newX2 , prefix1 , prefix2  ):
 	kmeans = KMeans(n_clusters=2, random_state=0 , init = "k-means++")
